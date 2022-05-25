e2361e7865fc5f7ddc1da251b71092a817371f02
Luca Lattanzio <luca.latnz@gmail.com>
2022-05-25 15:03:25 +0200

    fech 2.0

---
+++ MovieApp/config.js
@@ -0,0 +1,9 @@
+const API_KEY = "4b925bd5";
+export const URL_BASE = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
+
+export const content_type = {
+    MOVIE: "movie",
+    SERIES: "series",
+    EPISODE: "episode",
+    GAME: "game"
+}
--- MovieApp/js/modulo-api.js
+++ MovieApp/js/modulo-api.js
@@ -1,7 +1,22 @@
-export const url_movie = "http://www.omdbapi.com/?apikey=4b925bd5&s=terminator";
+import {URL_BASE} from "../config.js";
 
+export const lista_api = (s, type) => {
+    const url = URL_BASE + `s=${s}&type=${type}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(results => {
+            const items = results.Search;
+            console.log(items);
+        });
+};
+
+//------------------------------------------
+
+/*export const url_movie = "http://www.omdbapi.com/?apikey=4b925bd5&s=terminator";
+export const url_series = "http://www.omdbapi.com/?apikey=4b925bd5&s=terminator&type=series";
 export const somma = (a, b) => a+b;
 
+// Funzione che chiama la web api con l'elenco di tutti i contenuti di Terminator
 export const listMovie = () => {
     fetch(url_movie)
         .then (response => response.json())
@@ -10,3 +25,14 @@ export const listMovie = () => {
             console.log(movies);
         });
 };
+
+// Funzione che chiama la web api con l'elenco delle serie di Terminator
+export const listSerie = () => {
+    fetch(url_series)
+        .then (response => response.json())
+        .then(results => {
+            const series = results.Search;
+            console.log(series);
+        });
+};
+*/
