31f17d72271629fa1ba2361b83409cf5a5cbf36d
Luca Lattanzio <luca.latnz@gmail.com>
2022-05-25 10:52:59 +0200

    funzione fetch

--- MovieApp/js/modulo-api.js
+++ MovieApp/js/modulo-api.js
@@ -1,3 +1,12 @@
-export const pippo = "http://www.omdbapi.com/?apikey=4b925bd5&s=terminator";
+export const url_movie = "http://www.omdbapi.com/?apikey=4b925bd5&s=terminator";
 
 export const somma = (a, b) => a+b;
+
+export const listMovie = () => {
+    fetch(url_movie)
+        .then (response => response.json())
+        .then(results => {
+            const movies = results.Search;
+            console.log(movies);
+        });
+};
