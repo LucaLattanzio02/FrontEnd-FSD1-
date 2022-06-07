aba27f9f37b8bd38b6d613b87247ec3a44df193f
Luca Lattanzio <luca.latnz@gmail.com>
2022-06-07 23:31:48 +0200

    home.html V2 + modifiche

--- MovieApp/config.js
+++ MovieApp/config.js
@@ -1,5 +1,5 @@
 const API_KEY = "4b925bd5";
-export const URL_BASE = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
+export const URL_BASE = `https://www.omdbapi.com/?apikey=${API_KEY}&`;
 
 export const content_type = {
     MOVIE: "movie",
--- MovieApp/js/modulo-api.js
+++ MovieApp/js/modulo-api.js
@@ -1,62 +1,313 @@
 import {URL_BASE} from "../config.js";
 
-export const lista_api = (s, type, y) => {
-    const url = URL_BASE + `s=${s}&type=${type}&y=${y}`;
+export const PosterCarosello = (t, type) => {
+    const url = URL_BASE + `t=${t}&type=${type}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            const item = result;
+            
+            viewItemsCarosello(item); // richiamo della funzione "viewItems"
+        });
+};
+
+const viewItemsCarosello = (item) =>{
+   replacePosterCarousel(item);
+}
+
+const replacePosterCarousel = (movie) => {
+    const img = document.getElementById("carosello-img-1");
+
+    img.src = `${movie.Poster}`;
+}
+
+export const RicercaFilm = (t, type) => {
+    const url = URL_BASE + `t=${t}&type=${type}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            const item = result;
+            
+            viewItems(item); // richiamo della funzione "viewItems"
+        });
+};
+
+const viewItems = (item) =>{
+   replaceMovieContent(item);
+}
+
+const replaceMovieContent = (movie) => {
+
+    const element = document.getElementById("card-titolo-1");
+    const titolo_vecchio = document.getElementById("film-titolo-1");
+    const desc_vecchia = document.getElementById("desc-film-1");
+    const img_vecchia = document.getElementById("card-img-1");
+
+    const titolo = document.createElement("h5");
+    const node = document.createTextNode(movie.Title);
+    titolo.appendChild(node);
+
+    const para = document.createElement("p");
+    const node2 = document.createTextNode(movie.Plot);
+    para.appendChild(node2);
+
+    element.replaceChild(titolo, titolo_vecchio);
+    element.replaceChild(para, desc_vecchia);
+    img_vecchia.src = `${movie.Poster}`;
+}
+
+export const RicercaFilm2 = (t, type) => {
+    const url = URL_BASE + `t=${t}&type=${type}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            const item = result;
+            
+            viewItems2(item); // richiamo della funzione "viewItems"
+        });
+};
+
+const viewItems2 = (item) =>{
+   replaceMovieContent2(item);
+}
+
+const replaceMovieContent2 = (movie) => {
+
+    const element = document.getElementById("card-titolo-2");
+    const titolo_vecchio = document.getElementById("film-titolo-2");
+    const desc_vecchia = document.getElementById("desc-film-2");
+    const img_vecchia = document.getElementById("card-img-2");
+
+    const titolo = document.createElement("h5");
+    const node = document.createTextNode(movie.Title);
+    titolo.appendChild(node);
+
+    const para = document.createElement("p");
+    const node2 = document.createTextNode(movie.Plot);
+    para.appendChild(node2);
+
+    element.replaceChild(titolo, titolo_vecchio);
+    element.replaceChild(para, desc_vecchia);
+    img_vecchia.src = `${movie.Poster}`;
+}
+
+export const RicercaFilm3 = (t, type) => {
+    const url = URL_BASE + `t=${t}&type=${type}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            const item = result;
+            
+            viewItems3(item); // richiamo della funzione "viewItems"
+        });
+};
+
+const viewItems3 = (item) =>{
+   replaceMovieContent3(item);
+}
+
+const replaceMovieContent3 = (movie) => {
+
+    const element = document.getElementById("card-titolo-3");
+    const titolo_vecchio = document.getElementById("film-titolo-3");
+    const desc_vecchia = document.getElementById("desc-film-3");
+    const img_vecchia = document.getElementById("card-img-3");
+
+    const titolo = document.createElement("h5");
+    const node = document.createTextNode(movie.Title);
+    titolo.appendChild(node);
+
+    const para = document.createElement("p");
+    const node2 = document.createTextNode(movie.Plot);
+    para.appendChild(node2);
+
+    element.replaceChild(titolo, titolo_vecchio);
+    element.replaceChild(para, desc_vecchia);
+    img_vecchia.src = `${movie.Poster}`;
+}
+
+export const RicercaFilm4 = (t, type) => {
+    const url = URL_BASE + `t=${t}&type=${type}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            const item = result;
+            
+            viewItems4(item); // richiamo della funzione "viewItems"
+        });
+};
+
+const viewItems4 = (item) =>{
+   replaceMovieContent4(item);
+}
+
+const replaceMovieContent4 = (movie) => {
+
+    const element = document.getElementById("card-titolo-4");
+    const titolo_vecchio = document.getElementById("film-titolo-4");
+    const desc_vecchia = document.getElementById("desc-film-4");
+    const img_vecchia = document.getElementById("card-img-4");
+
+    const titolo = document.createElement("h5");
+    const node = document.createTextNode(movie.Title);
+    titolo.appendChild(node);
+
+    const para = document.createElement("p");
+    const node2 = document.createTextNode(movie.Plot);
+    para.appendChild(node2);
+
+    element.replaceChild(titolo, titolo_vecchio);
+    element.replaceChild(para, desc_vecchia);
+    img_vecchia.src = `${movie.Poster}`;
+}
+
+export const RicercaFilm5 = (t, type) => {
+    const url = URL_BASE + `t=${t}&type=${type}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            const item = result;
+            
+            viewItems5(item); // richiamo della funzione "viewItems"
+        });
+};
+
+const viewItems5 = (item) =>{
+   replaceMovieContent5(item);
+}
+
+const replaceMovieContent5 = (movie) => {
+
+    const element = document.getElementById("card-titolo-5");
+    const titolo_vecchio = document.getElementById("film-titolo-5");
+    const desc_vecchia = document.getElementById("desc-film-5");
+    const img_vecchia = document.getElementById("card-img-5");
+
+    const titolo = document.createElement("h5");
+    const node = document.createTextNode(movie.Title);
+    titolo.appendChild(node);
+
+    const para = document.createElement("p");
+    const node2 = document.createTextNode(movie.Plot);
+    para.appendChild(node2);
+
+    element.replaceChild(titolo, titolo_vecchio);
+    element.replaceChild(para, desc_vecchia);
+    img_vecchia.src = `${movie.Poster}`;
+}
+
+export const RicercaFilm6 = (t, type) => {
+    const url = URL_BASE + `t=${t}&type=${type}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            const item = result;
+            
+            viewItems6(item); // richiamo della funzione "viewItems"
+        });
+};
+
+const viewItems6 = (item) =>{
+   replaceMovieContent6(item);
+}
+
+const replaceMovieContent6 = (movie) => {
+
+    const element = document.getElementById("card-titolo-6");
+    const titolo_vecchio = document.getElementById("film-titolo-6");
+    const desc_vecchia = document.getElementById("desc-film-6");
+    const img_vecchia = document.getElementById("card-img-6");
+
+    const titolo = document.createElement("h5");
+    const node = document.createTextNode(movie.Title);
+    titolo.appendChild(node);
+
+    const para = document.createElement("p");
+    const node2 = document.createTextNode(movie.Plot);
+    para.appendChild(node2);
+
+    element.replaceChild(titolo, titolo_vecchio);
+    element.replaceChild(para, desc_vecchia);
+    img_vecchia.src = `${movie.Poster}`;
+}
+
+export const RicercaFilmSpin1 = (t, type) => {
+    const url = URL_BASE + `t=${t}&type=${type}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            const item = result;
+            
+            viewItemsSpinn(item); // richiamo della funzione "viewItems"
+        });
+};
+
+const viewItemsSpinn = (item) =>{
+   replaceSpinnContent(item);
+}
+
+const replaceSpinnContent = (movie) => {
+    const img_vecchia = document.getElementById("card-image-spinoff1");
+
+    img_vecchia.src = `${movie.Poster}`;
+}
+
+export const RicercaFilmSpin2 = (t, type) => {
+    const url = URL_BASE + `t=${t}&type=${type}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            const item = result;
+            
+            viewItemsSpinn2(item); // richiamo della funzione "viewItems"
+        });
+};
+
+const viewItemsSpinn2 = (item) =>{
+   replaceSpinnContent2(item);
+}
+
+const replaceSpinnContent2 = (movie) => {
+    const img_vecchia = document.getElementById("card-image-spinoff2");
+
+    img_vecchia.src = `${movie.Poster}`;
+}
+
+
+
+
+/*
+// per ricerca con s
+export const lista_api_s = (s, type) => {
+    const url = URL_BASE + `s=${s}&type=${type}`;
     fetch(url)
         .then(response => response.json())
         .then(results => {
             const items = results.Search;
             
-            viewItems(items); // richiamo della funzione "viewItems"
+            secondaFetch(items);
         });
 };
 
+const secondaFetch = (listaFilm) =>{
+    listaFilm.map((film) => {
 
-const viewItems = (items) =>{
+        let url = URL_BASE + `t=${traduciTitolo(film.Title)}`;
 
-    const element = document.getElementById("divprova");
-
-     // Ciclare array + Estrapolare ogni item
-    items.map((item) => {
-        
-        element.appendChild(create(item));
-       
-    });
+        fetch(url)
+        .then(response => response.json())
+        .then(results => {
+            const risultato = results;
+            
+            viewItems_t(risultato);
+            
+        });
+    })
 }
 
-const create = (movie) => {
-    const para = document.createElement("p");
-    const node = document.createTextNode(movie.Title);
-    para.appendChild(node);
-
-    return para;
-}
-
-// richiamo della funzione "viewItems"
-
-//------------------------------------------
-
-/*export const url_movie = "http://www.omdbapi.com/?apikey=4b925bd5&s=terminator";
-export const url_series = "http://www.omdbapi.com/?apikey=4b925bd5&s=terminator&type=series";
-export const somma = (a, b) => a+b;
-
-// Funzione che chiama la web api con l'elenco di tutti i contenuti di Terminator
-export const listMovie = () => {
-    fetch(url_movie)
-        .then (response => response.json())
-        .then(results => {
-            const movies = results.Search;
-            console.log(movies);
-        });
-};
-
-// Funzione che chiama la web api con l'elenco delle serie di Terminator
-export const listSerie = () => {
-    fetch(url_series)
-        .then (response => response.json())
-        .then(results => {
-            const series = results.Search;
-            console.log(series);
-        });
-};
 */
+
+const traduciTitolo = (titolo) => {
+    const stringa = titolo.replace(/\s/g, "%20");  // Trasforma gli gli spazzzzi in %20 per ricerca migliore
+    return stringa;
+}
